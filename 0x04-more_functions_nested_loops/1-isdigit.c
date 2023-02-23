e42b43f5cdf15d47c5a4331fc94750d119c0d7bd
ahracodes <nadififatimaezzahra01@gmail.com>
2023-02-23 15:26:45 +0100

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - entry point
- * Description: 
- * Return: 0 or 1
+ * _isdigit - to detect digits
+ * @c: the input
+ *  
+ * Return: 1 if input is a digit, 0 if otherwise
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+
 	else
 		return (0);
 }
