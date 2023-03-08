0dc01f183c8cb57837893e7989dca58b478ce5ef
ahracodes <nadififatimaezzahra01@gmail.com>
2023-03-08 12:16:22 +0100

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,20 +3,20 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  *
- * @s: string of char
+ * @s: string of cha
  *
  * return: 0
  */
 
 void _puts_recursion(char *s)
 {
-
-	 if (*s == '\0')
-        {
-                return;
-                _putchar('\n');
-        }
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
