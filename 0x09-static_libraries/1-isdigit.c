75ef1c16c5a321deb7efc80e614e5973404e3fcf
ahracodes <nadififatimaezzahra01@gmail.com>
2023-04-10 21:12:47 +0000

    task 6 dir fix 2

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - to detect digits
+ * @c: the input
+ * Return: 1 if input is a digit, 0 if otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
