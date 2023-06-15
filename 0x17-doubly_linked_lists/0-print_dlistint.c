5b173be18781dc75e70b04a550fa44c0a3b0333a
ahracodes <nadififatimaezzahra01@gmail.com>
2023-06-15 13:34:49 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,11 +12,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x;
-	int node = h->n;
 
 	for (x = 0; h != NULL; x++)
 	{
-		printf("%d\n", node);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (x);
